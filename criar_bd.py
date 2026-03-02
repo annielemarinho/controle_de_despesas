cb2e8acfa52e7ec7108fc4214e022abf78a2f1b8
Anniele Marinho <annimarinho14@gmail.com>
2026-03-02 17:56:39 -0300

    Criando tabelas

--- criar_bd.py
+++ criar_bd.py
@@ -4,3 +4,17 @@ import sqlite3 as lite
 #criando conexão
 con = lite.connect('dados.db')
 
+#criando tabela categoria
+with con:
+    cur = con.cursor()
+    cur.execute("CREATE TABLE Categoria(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT)")
+
+#criando tabela receitas
+with con:
+    cur = con.cursor()
+    cur.execute("CREATE TABLE Receitas(id INTEGER PRIMARY KEY AUTOINCREMENT, categoria TEXT, adicionado_em DATE, valor DECIMAL)")
+
+#criando tabela gastos
+with con:
+    cur = con.cursor()
+    cur.execute("CREATE TABLE Gastos(id INTEGER PRIMARY KEY AUTOINCREMENT, categoria TEXT, retirado_em DATE, valor DECIMAL)")
